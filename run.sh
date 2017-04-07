abf3b473914d5eda1276e472575123da54d4f76f
Ron Haskins <ron.haskins@oxehealth.com>
2017-04-07 15:09:47 +0100

    fixed issues in conainer run.sh

--- run.sh
+++ run.sh
@@ -6,10 +6,25 @@ ORGANIZATION=`cat ./config/organization`
 if [ `docker images | grep $CONTAINER | wc -l` -eq 0 ] ;
 then 
     NAMESPACE=$ORGANIZATION/
+elif [ `docker images | grep $ORGANIZATION/$CONTAINER | wc -l` -eq 1 ] ;
+then
+    NAMESPACE=$ORGANIZATION/
 fi
 
+# Ensure that ./code/bin folder exits and is writeable
+# use 777 as some users have to run this script as sudo
+# as thier account is not part of the docker group
+# to fix that run `sudo usermod -aG docker $USER`
+# then log out and back in again and it will be possible
+# to call docker run without prefixing with sudo
+SHARED_PATH=`pwd`/code
+BUILD_PATH=$SHARED_PATH/bin
+
+mkdir -p  $BUILD_PATH
+chmod 777 $BUILD_PATH
+
 docker run \
-    -v `pwd`/code:/code \
+    -v $SHARED_PATH:/code \
     -w /code \
     -i -t $NAMESPACE$CONTAINER \
     bash
