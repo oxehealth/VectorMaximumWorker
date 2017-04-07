be7161e7a21e6d87646b5be654a6d46484c54c25
Ron Haskins <ron.haskins@oxehealth.com>
2017-04-07 13:03:15 +0100

    updated container run.sh to autodetect if container has been built localy

--- run.sh
+++ run.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
 CONTAINER=`cat ./config/container`
+ORGANIZATION=`cat ./config/organization`
+
+if [ `docker images | grep $CONTAINER | wc -l` -eq 0 ] ;
+then 
+    NAMESPACE=$ORGANIZATION/
+fi
 
 docker run \
     -v `pwd`/code:/code \
     -w /code \
-    -i -t $CONTAINER \
+    -i -t $NAMESPACE$CONTAINER \
     bash
